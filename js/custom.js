94ef498ed257803f50fd52a02b677c214f69df91
perborgen <perhb@hotmail.com>
2015-02-06 16:32:16 +0000

    cleaning up stuff

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
 	var j = 0;
-	current_reader = 'Nora';
-	id = "Nora";
-	voices_loaded = false;
-	characters = ['Thomas','Ioana','Tarik','Diego', 
-	'Hysterical','Bruce','Alice', 'Nora','Xander','Amelie','Kyoko'];
-	character_images = [];
+	var previous_voice = "Nora";
+	var current_voice = "Nora";
+	var voices_loaded = false;
+
+
+
+	//Setting up the Web Speech API
+	// event thats triggered when the voices are ready. Populates the voice selector with options
+	window.speechSynthesis.onvoiceschanged = function(){
+	voices = speechSynthesis.getVoices();
+	}
 
 	// adding an event listener to when the user clicks one of the audio-icons
 	$(document).on('click', ".audio_link" , function(){
-		console.log(".audio_link clicked");
 		if (window.speechSynthesis.speaking == true)
 		{
 			window.speechSynthesis.cancel();
@@ -22,76 +26,81 @@ $(document).ready(function(){
 		} 
 	});
 
-
-
-	$("#charSelector").on('click', '.image',function(){
-		id = $(this).attr('id');
-		console.log(id);
-		$("#"+current_reader).animate({height: "50px"});
-		$("#"+id).animate({height: "100px"})
-		current_reader = id;
-	});
-
-
-	voiceSelector = document.getElementById("voice");
-	charSelector = document.getElementById("charSelector");
-
-	function fetch_voices(){
-	voices.map(function(voice){
-		for (i=0;i<characters.length; i++)
-			if (voice.name == characters[i]) {
-				character_images.push('<li class="character"><img class="image" src="img/' + voice.name +'.jpg" id= ' + voice.name + '></li>');	
-		}
-	});
-
-	animationLoop();
-	voices_loaded = true;
-	}
-
-	function animationLoop(){
-			$("#" + id).css({height: "100px"});
-		setTimeout(function(){ 
-			$(character_images[0]).css({height:'100px;'});
-			$('#charSelector').append(character_images[j]);
-			j++;
-			if (j<character_images.length){
-			animationLoop();
-			}
-			else if (j = character_images.length) {
-
-			}
-		}, 50)
-	}
-
-	// event thats triggered when the voices are ready. Populates the voice selector with options
-
-	window.speechSynthesis.onvoiceschanged = function(){
-	console.log('onvoicechanges triggered');	
-	voices = speechSynthesis.getVoices();
-
-	}
-
-	$("#character_activator").click(function(){
-		$(this).animate({width: 'hide'}, 150 , function(){
-				fetch_voices();
-		});
-	});
-
-
-
 	// getting the web speech api to read the message, and fetching the choosen voice
 	function readHeadline(input){	
 	message = new SpeechSynthesisUtterance(input);	
 	message.voice = voices.filter(function(voice_alternative){ 
-		return voice_alternative.name == id; 
+		return voice_alternative.name == current_voice; 
 	})[0];
 	window.speechSynthesis.speak(message);
 	}
 
-	// list of categories
+
+	// clicking the "Languages" button. 
+	$("#character_activator").click(function(){
+		$(this).animate({width: 'hide'}, 150 , function(){
+				fetchVoices();
+		});
+	});
+
+	var characters = ['Diego','Thomas','Ioana','Tarik', 
+	'Hysterical','Bruce','Alice', 'Nora','Xander', 'Amelie','Kyoko'];
+
+	// Finding the available the character elements
+	function fetchVoices(){
+	voices.map(function(voice){
+		for (i=0;i<characters.length; i++)
+			if (voice.name == characters[i]) {
+				character_elements.push('<li class="character"><img class="image" src="img/' + voice.name +'.jpg" id= ' + voice.name + '></li>');	
+		}
+	});
+	renderCharacters();
+	voices_loaded = true;
+	}
+
+	var character_elements = [];
+
+	// Adding the characters to the HTML
+	function renderCharacters(){
+			$("#Nora").css({height: "100px"});
+		setTimeout(function(){
+			$('#charSelector').append(character_elements[j]);
+			j++;
+			if (j<character_elements.length){
+				renderCharacters();
+			}
+		}, 50)
+	}
+
+	// When user clicks on a character.
+	$("#charSelector").on('click', '.image',function(){
+		current_voice = $(this).attr('id');
+		$("#"+previous_voice).animate({height: "50px"});
+		$("#"+current_voice).animate({height: "100px"})
+		previous_voice = current_voice;
+	});
+
+	// list of categories to be displayed at default
 	var categories = ["technology","sport","politics"];
 	var articles = {};
 
+	// Ajax call to The Guradian API
+	function getArticles(category) {
+		$.ajax({
+			url: "http://content.guardianapis.com/search?q=" + category + "&api-key=v2jpnga8trgw9x4p3u84yvrw",
+			dataType: 'jsonp',
+			success: function(json){
+				articles[category] = json.response.results; // 
+				renderArticles(articles, category);
+			}
+		});
+	}
+
+	categories.map(function(category) {
+		getArticles(category);
+	})
+
+	// Adding the fetched elements to the page
 	function renderArticles(articles, category) {
 		var listItems = [];
 		articles[category].map(function(article){
@@ -113,22 +122,6 @@ $(document).ready(function(){
 		return;
 	}
 
-	function getArticles(category) {
-		// tech articles
-		$.ajax({
-			url: "http://content.guardianapis.com/search?q=" + category + "&api-key=v2jpnga8trgw9x4p3u84yvrw",
-			dataType: 'jsonp',
-			success: function(json){
-				articles[category] = json.response.results; // 
-				renderArticles(articles, category);
-			}
-		});
-	}
-
-	categories.map(function(category) {
-		getArticles(category);
-	})
-
 
 	// Adding tab functionality 
 	$("#add").click(function(){
@@ -145,7 +138,7 @@ $(document).ready(function(){
 				exists = false;
 			}		
 		}
-		
+
 		if (exists)
 		{
 			$("li").removeClass("active");
@@ -159,14 +152,10 @@ $(document).ready(function(){
 			categories.push(searchLower);
 			getArticles(searchLower);
 		}
-
 		$("#category").html("").append("#"+search);
-
-		console.log(categories);
 	});
 
 	// closing a tab functionality
-	
 	 $(document).on("click", ".fa-times",function(){  
 	  //for elements inserted via jquery, use this document thing.  
 	  
